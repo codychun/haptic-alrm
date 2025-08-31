6a6731e5ed84661ec0275438a73655e775711965
codychun <108173260+codychun@users.noreply.github.com>
2025-08-31 19:25:20 -0400

    index

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 // Main app entry point - imports the AlarmApp component
+import { registerRootComponent } from 'expo';
 import AlarmApp from './app';
 
 export default AlarmApp;
+registerRootComponent(AlarmApp);
